22385e60646f40a39422dae5b1fb6a3cea3fcc93
Javeed798 <karishmavahida@gmail.com>
2024-01-17 23:50:53 +0530

    Day_4 (Bubble Sort Ascending and Descending Done)

--- src/day_4/BubbleSort.java
+++ src/day_4/BubbleSort.java
@@ -1,7 +1,32 @@
 package day_4;
 
+import java.util.Scanner;
+
 public class BubbleSort {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
 
+        int[] bubbleSort = bubbleSort(arr);
+        for (int i = 0; i < bubbleSort.length; i++) {
+            System.out.print(bubbleSort[i] + " ");
+        }
+    }
+
+    public static int[] bubbleSort(int[] arr) {
+        for (int turn = 1; turn < arr.length; turn++) {
+            for (int i = 0; i < arr.length - turn; i++) {
+                if (arr[i] > arr[i+1]) {   // just interchange > or < here to get ascending or descending sort
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+        }
+        return arr;
     }
 }
