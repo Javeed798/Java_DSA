0f7295c30e1bad1cd583f8fe87edaf631cd8678c
Javeed798 <karishmavahida@gmail.com>
2024-02-18 16:21:34 +0530

    day_16 (Sliding Window)

--- src/day_16/SlidingWindow_Kth_Max_SubArraySum.java
+++ src/day_16/SlidingWindow_Kth_Max_SubArraySum.java
@@ -1,6 +1,9 @@
 package day_16;
 
 public class SlidingWindow_Kth_Max_SubArraySum {
+
+    // is the problem is about to find the sub arrays, sub strings and also with some minima and maxima we blindly go for using this sliding window technique
+    // let the fun begins. :)
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 440, 5, 6, 7, 8,3,4,5,6,66};
         int i = subArrayMaxSum(arr, 3);
